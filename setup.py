9dc1ba259b7cb91ddd336d4eece1626f0b50b5f7
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-09-03 16:21:39 -0300

    create setup.py for package

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    name="image-processing-package",
+    version="0.1.0",
+    author="Rafael Vasconcelos",
+    author_email="rafaelvas2011@hotmail.com",
+    description="Um pacote Python de processamento de imagens",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="my_github_repository_project_link",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
