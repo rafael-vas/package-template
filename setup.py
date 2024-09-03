a1926d29f95a4e355435f9c6ccadbf464ea0a7c7
Rafael Vasconcelos <rafaelvas2011@hotmail.com>
2024-09-03 18:28:36 -0300

    add project url

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Um pacote Python de processamento de imagens",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link",
+    url="https://github.com/rafael-vas/package-template",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
